8fe0bd212d78be6a97aba5aa791d9ee9aaaa4f59
mohamedNaiem2000 <mohamednaiem735@gmail.com>
2023-03-13 17:21:17 +0200

    problem 3

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
-problem=[
-    {  "number":1,"title":"Even Or Odd" },
-    {  "number":2,"title":"Even" },
-    {  "number":2,"title":"Even" },
-    {  "number":2,"title":"Even" },
-    {  "number":2,"title":"Even" }, 
-]
-let btn1=document.querySelector(".btn1");
+problem = [
+  { number: 1, title: "Even Or Odd" },
+  { number: 2, title: "sum without negative" },
+  { number: 2, title: "Even" },
+  { number: 2, title: "Even" },
+  { number: 2, title: "Even" },
+];
+let btn1 = document.querySelector(".btn1");
 console.log(btn1);
-let result= problem.map((obj)=>{
-    return `<div>
-          number-  ${ obj.number}
+let solve = problem.map((obj) => {
+  return `<div>
+          number-  ${obj.number}
           name-  ${obj.title}
              <button>finish</button>
           </div>
-           `
+           `;
 });
-document.write(result)
+document.write(solve);
 /*----------------------------------------*/
 //problem 1
 // function evenorodd(value){
@@ -35,21 +35,43 @@ document.write(result)
 // console.log(evorod(5));
 /*--------------------------------------*/
 //problem 2
-let sum=0;
-function sumposetive(arr){
-for (let i = 0; i < arr.length; i++) {
-    if(arr[i]>0){
-        sum += arr[i];
-    }   
-}
-console.log(sum);
-}
-sumposetive([5,6,-7,8,9,-4]);
-//another solution
-function sumition(arr){
-    return arr.filter((x)=>x>1).reduce((acc,current)=>acc+current,0)
-}
+// let sum=0;
+// function sumposetive(arr){
+// for (let i = 0; i < arr.length; i++) {
+//     if(arr[i]>0){
+//         sum += arr[i];
+//     }
+// }
+// console.log(sum);
+// }
+// sumposetive([5,6,-7,8,9,-4]);
 
-console.log(sumition([1,2,3,-3,6,-9]));
+// //another solution
+
+// function sumition(arr){
+//     return arr.filter((x)=>x>1).reduce((acc,current)=>acc+current,0)
+// }
+
+// console.log(sumition([1,2,3,-3,6,-9]));
 /*------------------------------------------*/
+//problem3
+let result = 0;
 
+function sumwithouthm(arr) {
+  if (arr == null) return 0;
+  return arr
+    .filter((x) => x !== Math.min(...arr) && x !== Math.max(...arr))
+    .reduce((acc, current) => acc + current, 0);
+}
+console.log(sumwithouthm([1, 6, 8, 2,9,12]));
+//another solution
+function sumouthm(arr) {
+    let maxvalue = Math.max(...arr);
+    let minvalue = Math.min(...arr);
+    let filterarr = arr.filter((x) => x !== maxvalue && x !== minvalue);
+    let result=filterarr.reduce((acc, current) => acc + current, 0);
+    console.log(result);
+  }
+  
+  // }
+  sumouthm([1, 6, 8, 2,9,12]);
